a289b5ed1bc539f994ace3a09813a641d24276b8
cech.castellon <47920140+cechgamer@users.noreply.github.com>
2019-03-30 11:00:34 -0400

    commit con modelo MVC

---
+++ parcial/MVCparcial/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Data.Entity;
+
+
+namespace MVCparcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<MVCparcial.Models.Product> Products { get; set; }
+    }
+}
---
+++ parcial/MVCparcial/Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCparcial.Models
+{
+
+
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+
+    }
+
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string  Nombre { get; set; }
+
+        
+        public decimal Precio { get; set; }
+
+
+        [Required]
+        public int  Cantidad { get; set; }
+
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+
+
+    }
+}
